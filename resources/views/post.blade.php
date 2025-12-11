d679fbdab639ca9ef9c494b97ac348822f52abc8
AMAR_B <AMAR_B@DESKTOP-843Q2GH>
2025-12-11 08:40:33 -0800

    Modify the navigation and add the table for the feature

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -10,44 +10,82 @@
 
 
 
-<button type="button" class="btn btn-link">Link</button>
   </head>
   <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
+  <a class="navbar-brand" href="#">Ammar Bawaizr Blog Post </a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Dropdown
-          </a>
-          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" href="#">Action</a>
-            <a class="dropdown-item" href="#">Another action</a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">Something else here</a>
-          </div>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#">Disabled</a>
-        </li>
+      <a class="navbar-brand" href="#">  All Posts </a>
+
+
       </ul>
-      <form class="form-inline my-2 my-lg-0">
-        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-      </form>
+
     </div>
   </nav>
+  <div class="mt-5">
+    <div class="text-center" >
+      <button type="button" class="btn btn-success">Create</button>
+
+    </div>
+  </div>
+  <table class="table mt-4" >
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Title</th>
+        <th scope="col">Posted By</th>
+        <th scope="col">Created At</th>
+        <th scope="col">Actions</th>
+
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th scope="row">1</th>
+        <td>Mark</td>
+        <td>Otto</td>
+        <td>@mdo</td>
+        <td>
+          <div class="btn-group" role="group" aria-label="Post actions">
+            <button type="button" class="btn btn-primary btn-sm">View</button>
+            <button type="button" class="btn btn-warning btn-sm">Edit</button>
+            <button type="button" class="btn btn-danger btn-sm">Delete</button>
+          </div>
+        </td>
+      </tr>
+      <tr>
+        <th scope="row">2</th>
+        <td>Jacob</td>
+        <td>Thornton</td>
+        <td>@fat</td>
+        <td>
+          <div class="btn-group" role="group" aria-label="Post actions">
+            <button type="button" class="btn btn-primary btn-sm">View</button>
+            <button type="button" class="btn btn-warning btn-sm">Edit</button>
+            <button type="button" class="btn btn-danger btn-sm">Delete</button>
+          </div>
+        </td>
+      </tr>
+      <tr>
+        <th scope="row">3</th>
+        <td>Larry</td>
+        <td>the Bird</td>
+        <td>@twitter</td>
+        <td>
+          <div class="btn-group" role="group" aria-label="Post actions">
+            <button type="button" class="btn btn-primary btn-sm">View</button>
+            <button type="button" class="btn btn-warning btn-sm">Edit</button>
+            <button type="button" class="btn btn-danger btn-sm">Delete</button>
+          </div>
+        </td>
+      </tr>
+    </tbody>
+  </table>
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
